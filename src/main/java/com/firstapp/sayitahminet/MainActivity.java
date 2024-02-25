3659e43a982a027cb08dc1b0e4c7ca6ca2219363
Gorur56 <sevim.gorur01@gmail.com>
2024-02-25 22:05:52 +0300

    astimate page design

--- src/main/java/com/firstapp/sayitahminet/MainActivity.java
+++ src/main/java/com/firstapp/sayitahminet/MainActivity.java
@@ -3,12 +3,23 @@ package com.firstapp.sayitahminet;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button buttonBasla;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonBasla = findViewById(R.id.buttonBaslat);
+
+        buttonBasla.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
