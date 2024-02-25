c0308323adfaaa5bf86a324ad37e5b5fb70d5451
Gorur56 <sevim.gorur01@gmail.com>
2024-02-25 22:32:43 +0300

    astimate page activity

--- src/main/java/com/firstapp/sayitahminet/EstimateActivity.java
+++ src/main/java/com/firstapp/sayitahminet/EstimateActivity.java
@@ -3,12 +3,25 @@ package com.firstapp.sayitahminet;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class EstimateActivity extends AppCompatActivity {
+    private TextView textViewKalanHak, textViewYardim;
+    private EditText editTextGirdi;
+    private Button buttonTahmin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_estimate);
+
+        textViewKalanHak = findViewById(R.id.textViewKalanHak);
+        textViewYardim = findViewById(R.id.textViewYardim);
+
+        editTextGirdi = findViewById(R.id.editTextGirdi);
+
+        buttonTahmin = findViewById(R.id.buttonTahmin);
     }
 }
