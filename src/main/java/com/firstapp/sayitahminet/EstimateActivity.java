6ddc5ec1ea740bf4ae742bf7289d6bdb5a26fbdb
Gorur56 <sevim.gorur01@gmail.com>
2024-02-26 20:47:59 +0300

    estimate kalan hak

--- src/main/java/com/firstapp/sayitahminet/EstimateActivity.java
+++ src/main/java/com/firstapp/sayitahminet/EstimateActivity.java
@@ -4,15 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.Random;
+
 public class EstimateActivity extends AppCompatActivity {
     private TextView textViewKalanHak, textViewYardim;
     private EditText editTextGirdi;
     private Button buttonTahminnn;
+
+    private int rastgeleSayi;
+    private int sayac = 5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,11 +32,48 @@ public class EstimateActivity extends AppCompatActivity {
 
         buttonTahminnn = findViewById(R.id.buttonTahmin);
 
+        Random r = new Random();
+
+        rastgeleSayi = r.nextInt(101);
+        Log.e("Rastgele SAyı: ", String.valueOf(rastgeleSayi));
+
         buttonTahminnn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(EstimateActivity.this, ResultActivity.class));
-                finish();
+                sayac = sayac -1;
+
+                int tahmin = Integer.parseInt(editTextGirdi.getText().toString());
+
+                if ( tahmin == rastgeleSayi)
+                {
+                    Intent i = new Intent(EstimateActivity.this, ResultActivity.class);
+                    i.putExtra("sonuc",true);
+                    startActivity(i);
+                    finish();
+                    return;
+                }
+
+                if (tahmin > rastgeleSayi)
+                {
+                    textViewYardim.setText("Sayınızı Azaltınız ...");
+                    textViewKalanHak.setText("Kalan Hak: "+sayac);
+                }
+
+                if (tahmin < rastgeleSayi)
+                {
+                    textViewYardim.setText("Sayınızı yükseltiniz...");
+                    textViewKalanHak.setText("Kalan Hak: "+ sayac );
+                }
+
+                if(sayac <= 0)
+                {
+                    Intent i = new Intent(EstimateActivity.this, ResultActivity.class);
+                    i.putExtra("sonuc",false);
+                    startActivity(i);
+                    finish();
+                }
+
+                editTextGirdi.setText(""); //Tahmin ettğimiz sayı kalmasın
             }
         });
     }
